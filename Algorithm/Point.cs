27e028110b81bdf8ef0dd35fa14cd04bc0a038a0
AlinaYausheva <43522405+AlinaYausheva@users.noreply.github.com>
2023-04-19 19:17:33 +0300

    Update Point.cs

--- Algorithm/Point.cs
+++ Algorithm/Point.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,11 +8,67 @@ namespace Algorithm_Nelder_Mead
 {
     public class Point
     {
-        public double[] coordinate;
+        private double[] coordinate;
 
+        public double[] Coordinate { get { return coordinate; } set { coordinate = value; } }
         public Point(int n)
         {
             coordinate = new double[n];
+            for (int i = 0; i < n; i++)
+                coordinate[i] = 0;
         }
+
+        public double this[int index] { get { return coordinate[index]; } set { coordinate[index] = value; } }
+
+
+        public static Point operator /(Point point1, double number)
+        {
+            Point newPoint = new Point(point1.Coordinate.Length);
+            for (int i = 0; i < point1.Coordinate.Length; i++)
+            {
+                newPoint[i] = point1[i] / number;
+            }
+            return newPoint;
+        }
+
+        public static Point operator +(Point point1, Point point2)
+        {
+            Point newPoint = new Point(point1.Coordinate.Length);
+            for (int i = 0; i < point1.Coordinate.Length; i++)
+            {
+                newPoint[i] = point1[i] + point2[i];
+            }
+            return newPoint;
+        }
+
+        public static Point operator +(Point point1, double number)
+        {
+            Point newPoint = new Point(point1.Coordinate.Length);
+            for (int i = 0; i < point1.Coordinate.Length; i++)
+            {
+                newPoint[i] = point1[i] + number;
+            }
+            return newPoint;
+        }
+
+        public static Point operator *(double number, Point point1)
+        {
+            Point newPoint = new Point(point1.Coordinate.Length);
+            for (int i = 0; i < point1.Coordinate.Length; i++)
+            {
+                newPoint[i] = number * point1[i];
+            }
+            return newPoint;
+        }      
+
+        public static Point operator -(Point point1, Point point2)
+        {
+            Point newPoint = new Point(point1.Coordinate.Length);
+            for (int i = 0; i < point1.Coordinate.Length; i++)
+            {
+                newPoint[i] = point1[i] - point2[i];
+            }
+            return newPoint;
+        }       
     }
 }
