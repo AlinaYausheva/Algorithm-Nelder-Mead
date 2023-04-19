f2d537bcc3abb6f7324ef905eb63d0bf5ef906c6
AlinaYausheva <43522405+AlinaYausheva@users.noreply.github.com>
2023-04-19 19:15:06 +0300

    Update UnitTest1.cs

--- NelderMeadTests/UnitTest1.cs
+++ NelderMeadTests/UnitTest1.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace NelderMeadTests
 {
@@ -10,8 +11,13 @@ namespace NelderMeadTests
         {
             int[] expected = { 1, 1 };
 
+            Algorithm_Nelder_Mead.Point point = Algorithm_Nelder_Mead.NelderMead.Run("f(x1,x2)=(1-x1)^2+100*(x2-x1^2)^2", 1, 0.5, 2);
 
-            int[] result = Algorithm_Nelder_Mead.NelderMead.FunctionForTests("f(x1,x2)=(1-x1)^2+100*(x2-x1^2)^2", 1, 0.5, 2);
+
+            int[] result = new int[point.Coordinate.Length];
+
+            for (int i = 0; i < point.Coordinate.Length; i++)
+                result[i] = (int)Math.Round(point.Coordinate[i]);
 
             CollectionAssert.AreEqual(expected, result);
         }
@@ -20,7 +26,11 @@ namespace NelderMeadTests
         public void TestMethod2()
         {
             int[] expected = { 1, 4 };
-            int[] result = Algorithm_Nelder_Mead.NelderMead.FunctionForTests("f(x,y)=x^2+x*y+y^2-6*x-9*y", 1, 0.5, 2);
+            Algorithm_Nelder_Mead.Point point = Algorithm_Nelder_Mead.NelderMead.Run("f(x,y)=x^2+x*y+y^2-6*x-9*y", 1, 0.5, 2);
+            int[] result = new int[point.Coordinate.Length];
+
+            for (int i = 0; i < point.Coordinate.Length; i++)
+                result[i] = (int)Math.Round(point.Coordinate[i]);
 
             CollectionAssert.AreEqual(expected, result);
         }
@@ -29,7 +39,11 @@ namespace NelderMeadTests
         public void TestMethod3()
         {
             int[] expected = { -1, 0, 1 };
-            int[] result = Algorithm_Nelder_Mead.NelderMead.FunctionForTests("f(x1,x2,x3)=x1^2+x2^2+x3^2-x1*x2+x1-2*x3", 1, 0.5, 2);
+            Algorithm_Nelder_Mead.Point point = Algorithm_Nelder_Mead.NelderMead.Run("f(x1,x2,x3)=x1^2+x2^2+x3^2-x1*x2+x1-2*x3", 1, 0.5, 2);
+            int[] result = new int[point.Coordinate.Length];
+
+            for (int i = 0; i < point.Coordinate.Length; i++)
+                result[i] = (int)Math.Round(point.Coordinate[i]);
 
             CollectionAssert.AreEqual(expected, result);
         }
