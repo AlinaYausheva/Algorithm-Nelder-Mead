8fd2015550d72f6e93324893db9fc8ae1b746016
AlinaYausheva <43522405+AlinaYausheva@users.noreply.github.com>
2023-04-19 19:18:07 +0300

    Update Program.cs

--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -1,26 +1,45 @@
-﻿using System;
+using System;
+using System.IO;
 using org.mariuszgromada.math.mxparser;
 
 namespace Algorithm_Nelder_Mead
 {
     class Program
     {
+        public static string[] ReadInformationFromFile()
+        {
+            string[] s = new string[4];
+            StreamReader f = new StreamReader(@"C:\Users\Alina\Desktop\универ\6 семестр\Методы оптимизации\ЛР1 2.0\Algorithm Nelder-Mead\TestFile.txt");
+            for(int i = 0; i < 4; i++)
+            {
+                s[i] = f.ReadLine();
+            }
+            return s;
+        }
+
+        public static string[] ReadInformationFromConsole()
+        {
+            string[] s = new string[4];
+            Console.WriteLine("Введите альфа (обычно 1):");
+            s[0] = Console.ReadLine();//коэффициент отражения
+            Console.WriteLine("Введите бета (обычно 0,5):");
+            s[1] = Console.ReadLine();//коэффициент сжатия
+            Console.WriteLine("Введите гамма (обычно 2):");
+            s[2] = Console.ReadLine();//коэффициент растяжения
+            Console.WriteLine("Введите функцию (пример ввода f(x1,x2)=3*x1+x2^2):");
+            s[3] = Console.ReadLine();
+            return s;
+        }
         static void Main()
         {
             bool isCallSuccessful = License.iConfirmNonCommercialUse("Alina"); //"лицензия" для библиотеки
-            bool approximation = false;
+            
+            string [] inform = ReadInformationFromConsole();
+            //string[] inform = ReadInformationFromFile();
+            Point ans = NelderMead.Run(inform[3], double.Parse(inform[0]), double.Parse(inform[1]), double.Parse(inform[2]));
 
-            NelderMead.ReadInformation();
-
-            NelderMead.BuildSimplex();//1 шаг
-            while (!approximation)
-            {
-                NelderMead.PointSelection();//2 шаг
-                NelderMead.FindingGravityCenter();//3 шаг
-                NelderMead.Reflection();//4 шаг               
-                approximation = NelderMead.DerectionChecking();//5 шаг (внутри переход на шаг 6 и шаг 9)
-            }
-            NelderMead.WriteInformation();
+            Console.Write(NelderMead.WriteAnswer());
+            
         }
     }
 }
